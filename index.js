09d6bd62c08cee2ac4706da0129d55874480cbea
YOUNGGEUN OH <young@everon.co.kr>
2022-10-05 17:32:28 +0900

    1.6 데이터 처리에 해당하는 코드를 별도 파일로 분리

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const invoices = require("./invoices.json");
 const plays = require("./plays.json");
+const { createStatementData } = require("./createStatementData.js");
 
 // 리팩터링한 계산 함수들을 사용하여 HTML 결과를 출력하는 기능을 추가하고 싶음
 // 이를 위해 단개 쪼개기를 사용
@@ -8,69 +9,6 @@ const plays = require("./plays.json");
 
 function statement(invoice, plays) {
     return renderPlainText(createStatementData(invoice, plays));
-}
-
-// '데이터 처리'에 해당하는 코드를 모두 별도 함수로 빼냄
-function createStatementData(invoice, plays) {
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return statementData;
-
-    function enrichPerformance(aPerformance) {
-        const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
-        return result;
-    }
-
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
-    
-    function amountFor(aPerformance) {
-        let result = 0;
-
-        switch (aPerformance.play.type) {
-            case "tragedy": // 비극
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy": // 희극
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르 : ${playFor(aPerformance).type}`);
-        }
-
-        return result;
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-
-        if ("comedy" === aPerformance.play.type)
-            result += Math.floor(aPerformance.audience / 5);
-        return result;
-    }
-
-    function totalAmount(data) {
-        return data.performances.reduce((total, p) => total + p.amount, 0);
-    }
-
-    function totalVolumeCredits(data) {
-        return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-    }
 }
 
 function renderPlainText(data) {
@@ -91,7 +29,6 @@ function renderPlainText(data) {
         }).format(aNumber/100);
     }
 }
-
 
 // main
 invoices.forEach(invoice => {
