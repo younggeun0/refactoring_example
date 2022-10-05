eabd3843fadc2a11db51943ab6eb311537645486
YOUNGGEUN OH <young@everon.co.kr>
2022-10-05 16:24:56 +0900

    1.4 변수명 변경

--- index.js
+++ index.js
@@ -16,19 +16,19 @@ function statement(invoice, plays) {
 
     // 변수를 인라인해서 임시변수 제거 후 의미있게 함수명 수정
     function totalAmount() {
-        let totalAmount = 0;
+        let result = 0;
         for (let perf of invoice.performances) {
-            totalAmount += amountFor(perf);
+            result += amountFor(perf);
         }
-        return totalAmount;
+        return result;
     }
 
     function totalVolumeCredits() {
-        let volumeCredits = 0;
+        let result = 0;
         for (let perf of invoice.performances) {
-            volumeCredits += volumeCreditsFor(perf);
+            result += volumeCreditsFor(perf);
         }
-        return volumeCredits;
+        return result;
     }
 
     function usd(aNumber) {
