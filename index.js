9fd5e1f247a917f00902d422deaddd89326a6073
YOUNGGEUN OH <young@everon.co.kr>
2022-10-05 17:20:06 +0900

    1.6 적립 포인트를 중간 데이터에서 가져오도록 변경

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function statement(invoice, plays) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
         result.amount = amountFor(result); // 청구금액도 중간 데이터 구조에서 가져오도록 함
+        result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
 
@@ -46,6 +47,15 @@ function statement(invoice, plays) {
 
         return result;
     }
+
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+
+        if ("comedy" === aPerformance.play.type)
+            result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }
 }
 
 function renderPlainText(data) {
@@ -71,7 +81,7 @@ function renderPlainText(data) {
     function totalVolumeCredits() {
         let result = 0;
         for (let perf of data.performances) {
-            result += volumeCreditsFor(perf);
+            result += perf.volumeCredits;
         }
         return result;
     }
@@ -81,15 +91,6 @@ function renderPlainText(data) {
             style: "currency", currency: "USD",
             minimumFractionDigits: 2
         }).format(aNumber/100);
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-
-        if ("comedy" === aPerformance.play.type)
-            result += Math.floor(aPerformance.audience / 5);
-        return result;
     }
 }
 
