fd51dbd050ed97c23b4a9d0421748066edb6d0da
YOUNGGEUN OH <young@everon.co.kr>
2022-10-05 17:39:22 +0900

    1.6 중간 데이터 생성 함수를 공유하는 htmlStatement 버전추가

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const invoices = require("./invoices.json");
 const plays = require("./plays.json");
 const { createStatementData } = require("./createStatementData.js");
 
-// 리팩터링한 계산 함수들을 사용하여 HTML 결과를 출력하는 기능을 추가하고 싶음
-// 이를 위해 단개 쪼개기를 사용
-// 1. 필요한 데이터를 처리(계산 단계)
-// 2. 결과를 텍스트나 HTML로 표현(포맷팅 단계)
-
 function statement(invoice, plays) {
     return renderPlainText(createStatementData(invoice, plays));
 }
@@ -21,17 +16,38 @@ function renderPlainText(data) {
     result += `총액: ${usd(data.totalAmount)}\n`;
     result += `적립 포인트: ${data.totalVolumeCredits}\n`;
     return result;
+}
 
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US", {
-            style: "currency", currency: "USD",
-            minimumFractionDigits: 2
-        }).format(aNumber/100);
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+    let result = `<h1>청구내역 (고객명: ${data.customer})</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+    for (let perf of data.performances) {
+        result += ` <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
     }
+    result += "</table>\n";
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+    return result;
+}
+
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+        style: "currency", currency: "USD",
+        minimumFractionDigits: 2
+    }).format(aNumber/100);
 }
 
 // main
 invoices.forEach(invoice => {
     const result = statement(invoice, plays);
     console.log(result);
+
+    const result2 = htmlStatement(invoice, plays);
+    console.log(result2);
 });
