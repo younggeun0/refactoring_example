e95247a5bfa0a0d770c0aaab534afbbcc1866df6
YOUNGGEUN OH <young@everon.co.kr>
2022-10-05 17:21:37 +0900

    1.6 총액을 중간 데이터에서 가져오도록 변경

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
     return renderPlainText(statementData);
 
     function enrichPerformance(aPerformance) {
@@ -56,6 +58,22 @@ function statement(invoice, plays) {
             result += Math.floor(aPerformance.audience / 5);
         return result;
     }
+
+    function totalAmount(data) {
+        let result = 0;
+        for (let perf of data.performances) {
+            result += perf.amount;
+        }
+        return result;
+    }
+
+    function totalVolumeCredits(data) {
+        let result = 0;
+        for (let perf of data.performances) {
+            result += perf.volumeCredits;
+        }
+        return result;
+    }
 }
 
 function renderPlainText(data) {
@@ -65,26 +83,9 @@ function renderPlainText(data) {
         result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
     }
 
-    result += `총액: ${usd(totalAmount())}\n`;
-    result += `적립 포인트: ${totalVolumeCredits()}\n`;
+    result += `총액: ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트: ${data.totalVolumeCredits}\n`;
     return result;
-
-
-    function totalAmount() {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.amount;
-        }
-        return result;
-    }
-
-    function totalVolumeCredits() {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.volumeCredits;
-        }
-        return result;
-    }
 
     function usd(aNumber) {
         return new Intl.NumberFormat("en-US", {
