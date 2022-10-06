28748801129c5dd7ca32fc24213da4174e32c8a4
YOUNGGEUN OH <young@everon.co.kr>
2022-10-06 10:03:39 +0900

    1.8 타입 코드를 서브 클래스로 바꾸기
    다형성을 지원하도록 타입 코드를 서브클래스로 변경
    - PerformanceCalculator의 서브클래스들을 준비하고 createStatementData에서 적합한 서브클래스를 사용하게 해야 함
    - 적합한 서브클래스를 사용하려면 생성자 대신 함수를 호출하도록 변경(팩터리 함수)
    - JS에서는 생성자가 서브클래스의 인스턴스를 반환할 수 없기 때문에 "생성자를 팩터리 함수로 바꾸기" 사용

--- createStatementData.js
+++ createStatementData.js
@@ -38,6 +38,23 @@ class PerformanceCalculator {
     }
 }
 
+function createPerformanceCalculator(aPerformance, aPlay) {
+    switch (aPlay.type) {
+        case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+        default: {
+            throw new Error(`알 수 없는 장르 : ${aPlay.type}`);
+        }
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+
+}
+
+class ComedyCalculator extends PerformanceCalculator{
+
+}
 
 function createStatementData(invoice, plays) {
     const statementData = {};
@@ -48,7 +65,7 @@ function createStatementData(invoice, plays) {
     return statementData;
 
     function enrichPerformance(aPerformance) {
-        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
 
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
