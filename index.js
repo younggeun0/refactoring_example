7335a6121dfc98e862141683cbf5acc8a695472e
Young Oh <dureng5@gmail.com>
2023-04-29 12:07:18 +0900

    1.6 데이터 처리에 해당하는 코드를 모두 별도 함수로 빼냄

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const invoices = require("./invoices.json");
 const plays = require("./plays.json");
 
 function statement(invoice, plays) {
+    return renderPlainText(createStatementData(invoice, plays));
+}
+
+function createStatementData(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
     statementData.totalAmount = totalAmount(statementData);
     statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-
-    return renderPlainText(statementData, plays);
+    return statementData;
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
