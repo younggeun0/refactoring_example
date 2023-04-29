177fd7a0cb9ec25bae5b91cda58e763a7b91ba0d
Young Oh <dureng5@gmail.com>
2023-04-29 11:12:17 +0900

    1.4 변수 인라인하기

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ function statement(invoice, plays) {
     }).format;
 
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf);
-
         // 포인트를 적립한다.
         volumeCredits += Math.max(perf.audience - 30, 0);
 
@@ -20,8 +18,8 @@ function statement(invoice, plays) {
         if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
         // 청구 내역을 출력한다.
-        result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
-        totalAmount += thisAmount;
+        result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
+        totalAmount += amountFor(perf);
     }
 
     result += `총액: ${format(totalAmount / 100)}\n`;
