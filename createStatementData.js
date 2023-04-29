4e780ecc6ce940b86bcddffa8e4819b9eb3cc0fd
Young Oh <dureng5@gmail.com>
2023-04-29 13:08:03 +0900

    1.8 조건부 로직을 다형성으로 바꾸기

--- createStatementData.js
+++ createStatementData.js
@@ -5,38 +5,50 @@ class PerformanceCalculator {
     }
 
     get amount() {
-        let result = 0;
-
-        switch (this.play.type) {
-            case "tragedy": // 비극
-                result = 40000;
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30);
-                }
-                break;
-            case "comedy": // 희극
-                result = 30000;
-                if (this.performance.audience > 20) {
-                    result += 10000 + 500 * (this.performance.audience - 20);
-                }
-                result += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르 : ${this.play.type}`);
-        }
-
-        return result;
+        throw new Error("서브클래스에서 처리하도록 설계됨");
     }
 
     get volumeCredits() {
-        let result = Math.max(this.performance.audience - 30, 0);
-        if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
+        return Math.max(this.performance.audience - 30, 0);
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 0;
+        result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+        }
         return result;
     }
 }
 
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 0;
+        result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
+
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
+    }
+}
+
 function createPerformanceCalculator(aPerformance, aPlay) {
-    return new PerformanceCalculator(aPerformance, aPlay);
+    switch (aPlay.type) {
+        case "tragedy":
+            return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy":
+            return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error(`알 수 없는 장르 : ${this.play.type}`);
+    }
 }
 
 function createStatementData(invoice, plays) {
