eedbbfeefecb6308b7c5124d050920e659adbde4
Young Oh <dureng5@gmail.com>
2023-04-29 12:53:21 +0900

    1.8 조건부 로직을 다형성으로 바꾸기 시작

--- createStatementData.js
+++ createStatementData.js
@@ -1,3 +1,9 @@
+class PerformanceCalculator {
+    constructor(aPerformance) {
+        this.performance = aPerformance;
+    }
+}
+
 function createStatementData(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
@@ -7,6 +13,8 @@ function createStatementData(invoice, plays) {
     return statementData;
 
     function enrichPerformance(aPerformance) {
+        const calculator = new PerformanceCalculator(aPerformance);
+
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
         result.amount = amountFor(result);
