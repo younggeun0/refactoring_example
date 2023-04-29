c18d38c343732a58008d4b8e3b0bf89436ce83ba
Young Oh <dureng5@gmail.com>
2023-04-29 12:14:34 +0900

    1.6 데이터 처리에 해당하는 코드를 별도 파일로 분리

--- index.js
+++ index.js
@@ -1,70 +1,9 @@
+const { createStatementData } = require("./createStatementData");
 const invoices = require("./invoices.json");
 const plays = require("./plays.json");
 
 function statement(invoice, plays) {
     return renderPlainText(createStatementData(invoice, plays));
-}
-
-function createStatementData(invoice, plays) {
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return statementData;
-
-    function enrichPerformance(aPerformance) {
-        const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.credit = volumeCreditsFor(result);
-        return result;
-    }
-
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
-
-    function amountFor(aPerformance) {
-        let result = 0;
-
-        switch (aPerformance.play.type) {
-            case "tragedy": // 비극
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy": // 희극
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르 : ${aPerformance.play.type}`);
-        }
-
-        return result;
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        // 포인트를 적립한다.
-        let result = Math.max(aPerformance.audience - 30, 0);
-        // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-
-        return result;
-    }
-
-    function totalAmount(data) {
-        return data.performances.reduce((total, p) => total + p.amount, 0);
-    }
-
-    function totalVolumeCredits(data) {
-        return data.performances.reduce((total, p) => total + p.credit, 0);
-    }
 }
 
 function renderPlainText(data) {
@@ -87,7 +26,7 @@ function renderPlainText(data) {
     }
 }
 
-invoices.forEach(invoice => {
+invoices.forEach((invoice) => {
     const input1 = statement(invoice, plays);
     const result =
         "청구 내역 (고객명: BigCo)\n Hamlet: $650.00 (55석)\n As You Like it: $580.00 (35석)\n Othello: $500.00 (40석)\n총액: $1,730.00\n적립 포인트: 47\n";
