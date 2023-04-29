88b504086cc45adc8ac674232b9af02d70ae5725
Young Oh <dureng5@gmail.com>
2023-04-29 11:28:30 +0900

    1.4 반복문 쪼개기, 문장 슬라이드하기, 함수 추출하기(총액 구하는 함수, totalAmount), 변수 인라인

--- index.js
+++ index.js
@@ -2,16 +2,13 @@ const invoices = require("./invoices.json");
 const plays = require("./plays.json");
 
 function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-
     for (let perf of invoice.performances) {
         // 청구 내역을 출력한다.
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-        totalAmount += amountFor(perf);
     }
 
-    result += `총액: ${usd(totalAmount)}\n`;
+    result += `총액: ${usd(totalAmount())}\n`;
     result += `적립 포인트: ${totalVolumeCredits()}\n`;
     return result;
 
@@ -21,6 +18,14 @@ function statement(invoice, plays) {
             currency: "USD",
             minimumFractionDigits: 2,
         }).format(aNumber / 100);
+    }
+
+    function totalAmount() {
+        let results = 0;
+        for (let perf of invoice.performances) {
+            results += amountFor(perf);
+        }
+        return results;
     }
 
     function totalVolumeCredits() {
