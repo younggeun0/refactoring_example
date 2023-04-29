ba541c79de5a8798da2e4d2e464ac40234e3e67a
Young Oh <dureng5@gmail.com>
2023-04-29 11:15:56 +0900

    1.4 적립 포인트 계산 코드 함수로 추출(volumeCreditsFor)

--- index.js
+++ index.js
@@ -11,11 +11,7 @@ function statement(invoice, plays) {
     }).format;
 
     for (let perf of invoice.performances) {
-        // 포인트를 적립한다.
-        volumeCredits += Math.max(perf.audience - 30, 0);
-
-        // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+        volumeCredits = volumeCreditsFor(perf);
 
         // 청구 내역을 출력한다.
         result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
@@ -25,6 +21,15 @@ function statement(invoice, plays) {
     result += `총액: ${format(totalAmount / 100)}\n`;
     result += `적립 포인트: ${volumeCredits}\n`;
     return result;
+
+    function volumeCreditsFor(aPerformance) {
+        // 포인트를 적립한다.
+        let result = Math.max(aPerformance.audience - 30, 0);
+        // 희극 관객 5명마다 추가 포인트를 제공한다.
+        if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+
+        return result;
+    }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
