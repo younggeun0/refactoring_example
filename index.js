bf6a0f9e77d3773ca67134532e273e3a46249b63
Young Oh <dureng5@gmail.com>
2023-04-29 11:42:12 +0900

    1.6 기존 데이터 불변성을 지키기 위해 얕은 복사 수행

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const plays = require("./plays.json");
 function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances;
+    statementData.performances = invoice.performances.map(enrichPerformance);
 
-    renderPlainText(statementData, plays);
+    return renderPlainText(statementData, plays);
+
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance);
+        return result;
+    }
 }
 function renderPlainText(data, plays) {
     let result = `청구 내역 (고객명: ${data.customer})\n`;
