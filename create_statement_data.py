ade837cb5178858ac8b78e836d55f859b39d4008
Young Oh <dureng5@gmail.com>
2023-05-14 14:34:35 +0900

    조건부 로직을 다형성으로 바꾸기 시작

--- create_statement_data.py
+++ create_statement_data.py
@@ -1,36 +1,54 @@
+class PerformanceCalculator:
+    def __init__(self, a_performance, a_play):
+        self.performance = a_performance
+        self.play = a_play
+
+    # def amount(self):
+    #     raise NotImplementedError
+
+    # def volume_credits(self):
+    #     return max(self.performance['audience'] - 30, 0)
+
+    @property
+    def amount(self):
+        result = 0
+
+        if self.play['type'] == 'tragedy':
+            result = 40000
+            if self.performance['audience'] > 30:
+                result += 1000 * (self.performance['audience'] - 30)
+        elif self.play['type'] == 'comedy':
+            result = 30000
+            if self.performance['audience'] > 20:
+                result += 10000 + 500 * (self.performance['audience'] - 20)
+            result += 300 * self.performance['audience']
+        
+        return result 
+
+    @property
+    def volume_credits(self ):
+        result = max(self.performance['audience'] - 30, 0)
+
+        # 희극 관객 5명마다 추가 포인트를 제공한다.
+        if 'comedy' == self.play['type']:
+            result += self.performance['audience'] // 5
+
+        return result
+
+
 def create_statement_data(invoice, plays):
     def play_for(perf):
         return plays[perf['playID']]
     
-    def amount_for(perf):
-        result = 0
 
-        if play_for(perf)['type'] == 'tragedy':
-            result = 40000
-            if perf['audience'] > 30:
-                result += 1000 * (perf['audience'] - 30)
-        elif play_for(perf)['type'] == 'comedy':
-            result = 30000
-            if perf['audience'] > 20:
-                result += 10000 + 500 * (perf['audience'] - 20)
-            result += 300 * perf['audience']
-        
-        return result 
-
-    def volume_credits_for(perf):
-        result = max(perf['audience'] - 30, 0)
-
-        # 희극 관객 5명마다 추가 포인트를 제공한다.
-        if 'comedy' == play_for(perf)['type']:
-            result += perf['audience'] // 5
-
-        return result
 
     def enrich_performance(a_performance):
+        calculator = PerformanceCalculator(a_performance, play_for(a_performance))
+
         result = a_performance.copy()
-        result['play'] = play_for(result)
-        result['amount'] = amount_for(result)
-        result['volume_credits'] = volume_credits_for(result)
+        result['play'] = calculator.play
+        result['amount'] = calculator.amount
+        result['volume_credits'] = calculator.volume_credits
         return result
     
     def total_volume_credit(data):
