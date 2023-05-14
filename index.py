9527f684636be990d43dc4bab05cf034298d0fc3
Young Oh <dureng5@gmail.com>
2023-05-14 13:35:30 +0900

    변수 인라인하기

--- index.py
+++ index.py
@@ -46,8 +46,6 @@ def statement(invoice, plays):
 
     
     for perf in invoice['performances']:
-        this_amount = amount_for(perf)
-
         # 포인트를 적립한다.
         volume_credits += max(perf['audience'] - 30, 0)
 
@@ -56,8 +54,8 @@ def statement(invoice, plays):
             volume_credits += perf['audience'] // 5
 
         # 청구 내역을 출력한다.
-        result += f"  {play_for(perf)['name']}: ${this_amount/100:.2f} ({perf['audience']} 석)\n"
-        total_amount += this_amount
+        result += f"  {play_for(perf)['name']}: ${amount_for(perf)/100:.2f} ({perf['audience']} 석)\n"
+        total_amount += amount_for(perf)
 
     result += f"총액: ${total_amount/100:.2f}\n"
     result += f"적립 포인트: {volume_credits}\n"
